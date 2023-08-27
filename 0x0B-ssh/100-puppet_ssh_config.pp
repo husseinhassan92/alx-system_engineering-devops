27ef28d6395bfd3503b902056203cf7a4d508269
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-08-27 23:15:16 +0300

    task 0,1,2,3,4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,7 @@
 # Using puppet, create a file with below specifications
 
 file { 'configuration file':
+    path => '/etc/ssh/ssh_config',
     ensure  => file,
     content => 'Host 54.210.173.28
                   PasswordAuthentication no
