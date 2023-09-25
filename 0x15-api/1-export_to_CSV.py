758f2e9e68edf11a40703172cc023494727d54ef
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-09-25 16:22:35 +0300

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
 Request from API; Return TODO list progress given employee ID
-Export this data to CSV
 """
 import csv
 import requests
@@ -10,28 +9,22 @@ from sys import argv
 
 def to_csv():
     """return API data"""
-    users = requests.get("http://jsonplaceholder.typicode.com/users")
-    for u in users.json():
-        if u.get('id') == int(argv[1]):
-            USERNAME = (u.get('username'))
-            break
-    TASK_STATUS_TITLE = []
-    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
-    for t in todos.json():
-        if t.get('userId') == int(argv[1]):
-            TASK_STATUS_TITLE.append((t.get('completed'), t.get('title')))
+    r_users = requests.get("http://jsonplaceholder.typicode.com/users")
+    for user in r_users.json():
+        if user['id'] == int(argv[1]):
+            EMPLOYEE_NAME = user['username']
 
-    """export to csv"""
+    TASKS = []
+    r_todo = requests.get("https://jsonplaceholder.typicode.com/todos")
+    for todo in r_todo.json():
+        if todo['userId'] == int(argv[1]):
+            TASKS.append((todo['completed'], todo['title']))
+
     filename = "{}.csv".format(argv[1])
-    with open(filename, "w") as csvfile:
-        fieldnames = ["USER_ID", "USERNAME",
-                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
-                                quoting=csv.QUOTE_ALL)
-        for task in TASK_STATUS_TITLE:
-            writer.writerow({"USER_ID": argv[1], "USERNAME": USERNAME,
-                             "TASK_COMPLETED_STATUS": task[0],
-                             "TASK_TITLE": task[1]})
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for TASK in TASKS:
+            writer.writerow([int(argv[1]), EMPLOYEE_NAME, TASK[0], TASK[1]])
 
 
 if __name__ == "__main__":
