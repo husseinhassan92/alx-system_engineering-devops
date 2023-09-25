019ee06b98d81a6b5beade78a2204059d1ceab13
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-09-25 17:04:55 +0300

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Request from API; Return TODO dict progress given employee ID
+"""
+import json
+import requests
+from sys import argv
+
+
+def to_csv():
+    """return API data"""
+    r_users = requests.get("http://jsonplaceholder.typicode.com/users")
+    for user in r_users.json():
+        if user['id'] == int(argv[1]):
+            EMPLOYEE_NAME = user['username']
+
+    TASKS = []
+    r_todo = requests.get("https://jsonplaceholder.typicode.com/todos")
+    for todo in r_todo.json():
+        if todo['userId'] == int(argv[1]):
+            TASKS.append((todo['completed'], todo['title']))
+
+    list = []
+    for TASK in TASKS:
+        list.append({"task": TASK[1], "completed": TASK[0],
+                     "username": EMPLOYEE_NAME})
+
+    filename = "{}.json".format(argv[1])
+    with open(filename, 'w') as file:
+        for t in list:
+            json.dump({argv[1]: t}, file)
+
+
+if __name__ == "__main__":
+    to_csv()
