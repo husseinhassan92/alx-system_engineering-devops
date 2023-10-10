885b743b986b56fdb522a4ca2fa3d3f6e2623898
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-10-10 17:54:47 +0300

    task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Query Reddit API for number of subscribers for a given subreddit
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after='', count=0):
+    """"""
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}&count={}'.format(
+        subreddit, after, count)
+    headers = {'user-agent': 'My User Agent 1.0'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return None
+    else:
+        data = response.json().get('data')
+        after = data.get('after')
+        count += data.get('dist')
+        for t in data.get('children'):
+            hot_list.append(t.get('data').get('title'))
+        if after is not None:
+            return (recurse(subreddit, hot_list, after, count))
+        return hot_list
