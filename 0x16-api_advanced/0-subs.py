b058624767bf8513caa632226f7866df86bb643a
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-10-10 15:28:14 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,14 +8,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     """"""
-    url = 'https://www.reddit.com/r/{}/about'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'user-agent': 'My User Agent 1.0'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    data = response.json()
-    count = data.get('data').get('subscribers')
-
     if response.status_code == 404:
-        return 0
+        count = 0
+    else:
+        data = response.json()
+        count = data.get('data').get('subscribers')
     return count
